139b438838c5f13c6d57a348eefe1051340c4799
DanisValitov <fermodil@mail.ru>
2023-02-12 07:14:58 +0300

    add config

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-client-server images transferring 
+# client-server images transferring 
+
+#### for client config add server.csv to conf dir with content like:
+```
+host;port
+192.168.0.1;4444
+```
+---
--- src/client/udp_c.py
+++ src/client/udp_c.py
@@ -5,10 +5,22 @@ import time
 
 import cv2
 import imutils
+import csv
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.settimeout(1.0)
-addr = ("127.0.0.1", 12000)
+addr = ("127.0.0.1", 4444)
+
+try:
+    with open('../../conf/server.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        headers = next(reader, None)
+        conf = next(reader, None)
+        addr = (conf[0], int(conf[1]))
+except:
+    print("no conf found, using default")
+
+print(addr)
 BUFSIZE = 1024
 
 vid = cv2.VideoCapture(0)
@@ -19,7 +31,7 @@ while True:
     frame = imutils.resize(frame, width=640)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 60] # change number for imgencode size
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 60]  # change number for imgencode size
     result, imgencode = cv2.imencode(".jpg", frame, encode_param)
 
     a = pickle.dumps(imgencode)
